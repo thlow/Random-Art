8b1c61ec0ef46945dcdd68a3d832aba92f9b51f6
thlow <52280549+thlow@users.noreply.github.com>
2019-07-03 20:29:10 +0800

    Add files via upload

---
+++ Abstract Random.py
@@ -0,0 +1,81 @@
+tom = Turtle()
+tom.shape('turtle')
+tom.color('white')
+tom.width(5)
+tom.tracer(100)
+
+for count in range(150):
+    tom.left(53)
+    tom.forward(100)
+    tom.lt(74)
+    tom.fd(100)
+    tom.lt(90)
+    tom.fd(60)
+    tom.lt(100)
+    tom.fd(80)
+    
+tom.penup()
+tom.fd(120)
+tom.color('yellow')
+tom.pendown()
+
+for count in range(100):
+    tom.fd(100)
+    tom.lt(360/8)
+    tom.fd(10)
+    tom.lt(360/8)
+    tom.fd(10)
+    tom.lt(360/8)
+    tom.fd(100)
+    tom.lt(70)
+    
+tom.lt(90)
+tom.color('orange')
+for count in range(100):
+    tom.fd(100)
+    tom.lt(360/8)
+    tom.fd(10)
+    tom.lt(360/8)
+    tom.fd(10)
+    tom.lt(360/8)
+    tom.fd(100)
+    tom.lt(70)
+tom.lt(90)
+tom.color('red')
+for count in range(100):
+    tom.fd(100)
+    tom.lt(360/8)
+    tom.fd(10)
+    tom.lt(360/8)
+    tom.fd(10)
+    tom.lt(360/8)
+    tom.fd(100)
+    tom.lt(70)
+    
+tom.lt(90)
+tom.color('pink')
+for count in range(100):
+    tom.fd(100)
+    tom.lt(360/8)
+    tom.fd(10)
+    tom.lt(360/8)
+    tom.fd(10)
+    tom.lt(360/8)
+    tom.fd(100)
+    tom.lt(70)
+    
+tom.lt(90)
+tom.color('blue')
+for count in range(100):
+    tom.fd(100)
+    tom.lt(360/8)
+    tom.fd(10)
+    tom.lt(360/8)
+    tom.fd(10)
+    tom.lt(360/8)
+    tom.fd(100)
+    tom.lt(70)
+
+
+    
+
