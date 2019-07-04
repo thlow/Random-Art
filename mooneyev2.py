a5b80f36d9d71cc1b439fce9668f7954dbe83d31
thlow <52280549+thlow@users.noreply.github.com>
2019-07-04 12:57:38 +0800

    Add files via upload

---
+++ mooneyev2.py
@@ -0,0 +1,33 @@
+tom = Turtle()
+tom.shape('turtle')
+tom.color('light blue')
+tom.tracer(100)
+
+
+for count in range(100):
+    tom.lt(360/100)
+    for count in [1,2,3,4]:
+        tom.fd(100)
+        tom.rt(60)
+        tom.fd(100)
+        tom.rt(170)
+        tom.fd(100)
+        tom.rt(60)
+        tom.fd(100)
+
+tom.goto(-80,170)
+tom.color('white')
+        
+for count in range(100):
+    for count in [1,2,3,4]:
+        tom.fd(50)
+        tom.rt(60)
+        tom.fd(50)
+        tom.rt(170)
+        tom.fd(50)
+        tom.rt(60)
+        tom.fd(50)
+    
+    tom.rt(360.0/100)
+tom.penup()
+tom.lt(360)
