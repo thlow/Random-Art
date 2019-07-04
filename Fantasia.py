08e3c90fd0e2c6f334fcacc21806f6a694f33dc2
thlow <52280549+thlow@users.noreply.github.com>
2019-07-04 12:58:57 +0800

    Add files via upload

---
+++ Fantasia.py
@@ -0,0 +1,25 @@
+tom = Turtle()
+tom.shape('turtle')
+
+tom.speed(100)
+
+for count in range(50):
+    
+    tom.color('green')
+    
+    tom.forward(100)
+    tom.color('yellow')
+    tom.left(90)
+    tom.forward(100)
+    tom.color('orange')
+    tom.left(90)
+    tom.forward(100)
+    tom.color('red')
+    tom.right(90)
+    
+    tom.forward(100)
+    tom.color('purple')
+    tom.right(90)
+    tom.forward(100)
+    
+    tom.left(64)
