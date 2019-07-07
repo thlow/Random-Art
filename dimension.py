ddff12ea594d2f813c210f53008731f936331816
thlow <52280549+thlow@users.noreply.github.com>
2019-07-07 19:00:11 +0800

    Add files via upload

---
+++ dimension.py
@@ -0,0 +1,24 @@
+tom = Turtle()
+tom.shape('turtle')
+tom.color('green')
+tom.tracer(100)
+
+
+for count in range(50):
+    
+    tom.forward(70)
+    tom.color('yellow')
+    tom.left(90)
+    tom.forward(100)
+    tom.color('orange')
+    tom.left(70)
+    tom.forward(800)
+    tom.color('red')
+    tom.right(90)
+        
+    tom.forward(60)
+    tom.color('purple')
+    tom.right(90)
+    tom.forward(50)
+    tom.left(150)
+        
